27067f8dad4d09eb2635fdf121354f0ed12efe3c
dengzb <dengzb@ucweb.com>
2013-08-10 23:17:42 +0800

    1. optimize builldcscope.bat script 2. recompile cscope

--- plusin/vimSupport/builldcscope.bat
+++ plusin/vimSupport/builldcscope.bat
@@ -1,4 +1,12 @@
+rem if not exist %VIM_PATH%\..\vimSupport goto end
+rem setlocal
+rem PATH=%VIM_PATH%\..\vimSupport;%PATH%
+
 dir *.cpp *.h *.hpp *.c *.java *.cc /s /B > tmp.lst
 cscope -bqk -i tmp.lst
+ctags -R -L tmp.lst
 del tmp.lst
-ctags -R
+
+
+rem :end
+rem echo exit
